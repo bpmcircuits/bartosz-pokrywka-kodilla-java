920b5fc9b4e5c46254ea08ce1f9ed67a4e1e2ffb
Bartek Pokrywka <pokrywkamusic@gmail.com>
2025-02-05 01:13:00 +0100

    6.2 Added Unit Test for Calculator methods

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int addResult = Calculator.add(2, 3);
+
+        int subtractResult = Calculator.subtract(3, 2);
+
+        System.out.println("Testing addition: ");
+        if (addResult == 5) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Testing subtraction: ");
+        if (subtractResult == 1) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
