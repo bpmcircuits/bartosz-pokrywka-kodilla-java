d42f99130f2e5e10de23029680c1798bce0be062
Bartek Pokrywka <pokrywkamusic@gmail.com>
2025-02-12 17:06:43 +0100

    Correction for Exercise 7.5

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,7 +4,12 @@ import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static double getAverage(int[] numbers) {
+        IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .forEach(System.out::println);
+
         return IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
                 .average()
                 .orElse(0);
     }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -8,13 +8,13 @@ public class ArrayOperationsTestSuite {
     @Test
     void testArrayOperationsMethod() {
         //Given
-        int[] numbersToCalculate = new int[]{2,3,4,6,2,3,1,7,10};
+        int[] numbersToCalculate = new int[]{2,3,4,6,2,3,1,7,10,4,5,1,3,6,3,8,11,15,16,2};
 
         //When
         double averageResult = ArrayOperations.getAverage(numbersToCalculate);
 
         //Then
 
-        assertEquals(4.0, averageResult);
+        assertEquals(5.6, averageResult);
     }
 }
