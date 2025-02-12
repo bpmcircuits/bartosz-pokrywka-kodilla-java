174b3c74418b11983fcca17a5cd65c0a63214494
Bartosz Pokrywka <coverbart@gmail.com>
2025-02-12 11:09:04 +0100

    stream on any object

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,39 +1,13 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.beautifier.PoemDecorator;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.reference.FunctionalCalculator;
+
+import com.kodilla.stream.book.BookDirectory;
 
 public class StreamMain {
     public static void main(String[] args) {
-//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-//
-//        System.out.println("Calculating expressions with lambdas");
-//        expressionExecutor.executeExpression(10, 5, ((a, b) -> a + b));
-//        expressionExecutor.executeExpression(10, 5, ((a, b) -> a - b));
-//        expressionExecutor.executeExpression(10, 5, ((a, b) -> a * b));
-//        expressionExecutor.executeExpression(10, 5, ((a, b) -> a / b));
-//
-//        System.out.println("Calculating expressions with method reference");
-//        expressionExecutor.executeExpression(3, 6, FunctionalCalculator::addAToB);
-//        expressionExecutor.executeExpression(3, 6, FunctionalCalculator::subBFromA);
-//        expressionExecutor.executeExpression(3, 6, FunctionalCalculator::multiplyAByB);
-//        expressionExecutor.executeExpression(3, 6, FunctionalCalculator::divideAByB);
-
-//        PoemBeautifier poemBeautifier = new PoemBeautifier();
-//
-//        String textToBeautify = "This is a text for this experiment.";
-//        String textToBeautify2 = "Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.";
-//
-//        System.out.println(poemBeautifier.beautify(textToBeautify, text -> text.repeat(3)));
-//        System.out.println(poemBeautifier.beautify(textToBeautify2, String::toUpperCase));
-//        System.out.println(poemBeautifier.beautify(textToBeautify, text -> text.replace("t", "T")));
-//        System.out.println(poemBeautifier.beautify(textToBeautify, text -> text.substring(8, 18)));
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .forEach(System.out::println);
     }
 }
